c1de0e65c5640652ca692eb941e73c116ec7f93e
Bart Blok <bart@wittig.nl>
2015-08-20 22:31:39 +0200

    Collection: Implemented Underscore methods

--- src/TSData/Collection.ts
+++ src/TSData/Collection.ts
@@ -4,37 +4,89 @@ module TSData {
 
     export class Collection<T> {
 
-        data:[T];
+        private data:[T];
 
-        add(item:T) {
+        constructor(data?:[T]){
+            this.data = data || [];
+        }
+
+        public add(item:T) {
             this.data.push(item);
         }
 
-        buildModel(attrs):T {
-            return null;
+        public addAll(items:[T]){
+            this.data = this.data.concat(items);
         }
 
-        populate(items) {
+        public remove(item:T){
+            this.data = _.without(this.data, item);
+        }
 
-            _.each(items, (item) => {
+        public removeAll(items:[T]){
+            this.data = _.difference(this.data, items);
+        }
 
-                var model = this.buildModel(item);
+        public first():T {
+            return _.first(this.data);
+        }
 
+        public last():T {
+            return _.last(this.data);
+        }
+
+        public reset() {
+            this.data = [];
+        }
+
+        public each(iterator:_.ListIterator<T, void>){
+            _.each(this.data, iterator)
+        }
+
+        public pluck(propertyName:string):[any] {
+            return _.pluck(this.data, propertyName);
+        }
+
+        public count():number {
+            return this.data.length;
+        }
+
+        get length():number {
+            return this.count();
+        }
+
+        public populate(items) {
+
+            _.each(items, (itemData) => {
+
+                var model = this.createItem(itemData);
                 if (model) {
                     return this.add(model);
                 }
-
             });
         }
 
-        find():[T] {
-
-            return this.data;
+        public find(iterator:_.ListIterator<T, boolean>):[T] {
+            return _.find(this.data, iterator);
         }
 
-        findFirst():T {
+        public filter(iterator:_.ListIterator<T, boolean>):[T] {
+            return _.filter(this.data, iterator);
+        }
 
-            return this.data[0];
+        public where(properties:{}):[T] {
+            return _.where(this.data, properties);
+        }
+
+        public findWhere(properties:{}):T {
+            return _.findWhere(this.data, properties);
+        }
+
+        public toArray():[T] {
+            return _.clone(this.data);
+        }
+
+        protected createItem(itemData):T {
+            return itemData;
         }
     }
 }
