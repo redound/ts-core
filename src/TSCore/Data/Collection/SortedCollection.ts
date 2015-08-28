582be5fac3c9c91ec02fa97d77e1c884348e0dfb
Olivier Andriessen <olivierandriessen@gmail.com>
2015-08-28 20:23:32 +0200

    SortedCollection: add comments

--- src/TSCore/Data/Collection/SortedCollection.ts
+++ src/TSCore/Data/Collection/SortedCollection.ts
@@ -23,15 +23,29 @@ module TSCore.Data.Collection {
 
         protected _sortPredicate;
 
+        /**
+         * Magic getter for sortPredicate.
+         *
+         * @returns {any}
+         */
         public get sortPredicate() {
             return this._sortPredicate;
         }
 
+        /** Magic setter for sortPredicate
+         *
+         * @param predicate Predicate to set.
+         */
         public set sortPredicate(predicate) {
             this._sortPredicate = predicate;
             this.sort();
         }
 
+        /**
+         * Constructor function
+         * @param data Data to populate collection of instance with.
+         * @param sortPredicate Predicate to sort collection to.
+         */
         constructor(data:T[], sortPredicate) {
 
             super(data);
@@ -40,30 +54,60 @@ module TSCore.Data.Collection {
             this.sort();
         }
 
-        public add(item:T) {
+        /**
+         * Add item to collection.
+         *
+         * @param item Item to be added.
+         * @return {void}
+         */
+        public add(item:T): void {
 
             super.add(item);
             this.sort();
         }
 
-        public addMany(items:T[]) {
+        /**
+         * Add multiple items to collection.
+         *
+         * @param items Items to be added.
+         * @return {void}
+         */
+        public addMany(items:T[]): void {
 
             super.addMany(items);
             this.sort();
         }
 
-        public remove(item:T) {
+        /**
+         * Remove item from collection.
+         *
+         * @param item Item to be removed.
+         * @return {void}
+         */
+        public remove(item:T): void {
 
             super.remove(item);
             this.sort();
         }
 
+        /**
+         * Remove multiple items from collection.
+         *
+         * @param items Items to be removed.
+         */
         public removeMany(items:T[]) {
 
             super.removeMany(items);
             this.sort();
         }
 
+        /**
+         * Replace an item for another item.
+         *
+         * @param source Item to be replaced in collection.
+         * @param replacement Item that replaces source item in collection.
+         * @returns {T}
+         */
         public replaceItem(source:T, replacement:T): T {
 
             var currentItem = super.replaceItem(source, replacement);
@@ -72,23 +116,50 @@ module TSCore.Data.Collection {
             return currentItem;
         }
 
+        /**
+         * Get first item in collection.
+         *
+         * @returns {T}
+         */
         public first(): T {
             return _.first(this._data);
         }
 
+        /**
+         * Get last item in collection.
+         *
+         * @returns {T}
+         */
         public last(): T {
             return _.last(this._data);
         }
 
+        /**
+         * Get item at index.
+         *
+         * @param index Index to get item for.
+         * @returns {T}
+         */
         public get(index:number): T {
             return this._data[index];
         }
 
+        /**
+         * Get index of item.
+         *
+         * @param item Item to get index for.
+         * @returns {number}
+         */
         public indexOf(item:T): number {
             return _.indexOf(this._data, item);
         }
 
-        public sort() {
+        /**
+         * Sort collection.
+         *
+         * @returns {void}
+         */
+        public sort(): void {
 
             if(this._sortPredicate === null || this._sortPredicate === undefined) {
                 return;
