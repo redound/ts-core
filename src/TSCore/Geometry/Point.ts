ff78565d2830aefb91d90db0b62d762fbf7c8d2a
Olivier Andriessen <olivierandriessen@gmail.com>
2015-08-28 20:48:26 +0200

    Point: add comments

--- src/TSCore/Geometry/Point.ts
+++ src/TSCore/Geometry/Point.ts
@@ -5,12 +5,23 @@ module TSCore.Geometry {
         public x:number;
         public y:number;
 
+        /**
+         * Constructor function
+         * @param x Position value for x. Defaults to zero.
+         * @param y Position value for y. Defaults to zero.
+         */
         constructor(x:number=0, y:number=0) {
 
             this.x = x;
             this.y = y;
         }
 
+        /**
+         * Translate points values.
+         *
+         * @param x Value to increase x position with.
+         * @param y Value to increase y position with.
+         */
         public translate(x:number, y:number){
 
             this.x += x;
