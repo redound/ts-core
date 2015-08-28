306a0a8aaff8d155f0835522482ed914dabd3309
Olivier Andriessen <olivierandriessen@gmail.com>
2015-08-28 19:35:47 +0200

    Collection: comment

--- src/TSCore/Data/Collection/Collection.ts
+++ src/TSCore/Data/Collection/Collection.ts
@@ -25,11 +25,20 @@ module TSCore.Data.Collection {
 
         protected _data:T[];
 
-
+        /**
+         * Prepend item to collection.
+         *
+         * @param item  Item to be inserted.
+         */
         public prepend(item:T) {
             this.insert(item, 0);
         }
 
+        /**
+         * Prepend multiple items to collection.
+         *
+         * @param items Items to be inserted
+         */
         public prependMany(items:T[]) {
 
             this._data = items.concat(this._data);
@@ -38,6 +47,12 @@ module TSCore.Data.Collection {
             this.trigger(CollectionEvents.CHANGE);
         }
 
+        /**
+         * Insert an item at a certain index.
+         *
+         * @param item  Item to be inserted.
+         * @param index Index to insert item at.
+         */
         public insert(item:T, index:number){
 
             this._data.splice(index, 0, item);
@@ -46,10 +61,24 @@ module TSCore.Data.Collection {
             this.trigger(CollectionEvents.CHANGE);
         }
 
+        /**
+         * Replace one item with another item.
+         *
+         * @param source        The item that gets replaced inside the collection.
+         * @param replacement   The item that replaces the source item.
+         * @returns {T}
+         */
         public replaceItem(source:T, replacement:T): T {
             return this.replace(this.indexOf(source), replacement);
         }
 
+        /**
+         * Replace one item at a certain index.
+         *
+         * @param index         Index of the item that gets replaced.
+         * @param replacement   The item the replaces the source item.
+         * @returns {any}
+         */
         public replace(index:number, replacement:T): T {
 
             if(index < 0 || index >= this.count()){
@@ -65,18 +94,39 @@ module TSCore.Data.Collection {
             return currentItem;
         }
 
+        /**
+         * Get the first item from collection.
+         *
+         * @returns {T}
+         */
         public first(): T {
             return _.first(this._data);
         }
 
+        /**
+         * Get the last item from collection.
+         * @returns {T}
+         */
         public last(): T {
             return _.last(this._data);
         }
 
+        /**
+         * Get an item at a specified index in collection.
+         *
+         * @param index Index of the item to be returned.
+         * @returns {T}
+         */
         public get(index:number): T {
             return this._data[index];
         }
 
+        /**
+         * Get the index of an item in collection.
+         *
+         * @param item Item to return index for.
+         * @returns {number}
+         */
         public indexOf(item:T): number {
             return _.indexOf(this._data, item);
         }
