b59a962c233cd961e15a3e2b8d45d307d270fa7e
Olivier Andriessen <olivierandriessen@gmail.com>
2015-08-28 21:17:07 +0200

    Format: add comments

--- src/TSCore/Text/Format.ts
+++ src/TSCore/Text/Format.ts
@@ -11,6 +11,12 @@ module TSCore.Text {
             "/": '&#x2F;'
         };
 
+        /**
+         * Escape a html string.
+         *
+         * @param input String to be parsed.
+         * @returns {string}
+         */
         public static escapeHtml(input:string):string {
 
             var entityMap = Format.HtmlEntityMap;
@@ -20,6 +26,14 @@ module TSCore.Text {
             });
         }
 
+        /**
+         * Truncate strings length with a suffix for a given length.
+         *
+         * @param input             String to be truncated.
+         * @param maxLength         Length of the truncated string.
+         * @param suffix            Suffix to be added at the end of a string. Defaults to '...'.
+         * @returns {string}
+         */
         public static truncate(input:string, maxLength:number, suffix:string = '...'):string {
 
             if (input.length <= length) {
@@ -29,7 +43,15 @@ module TSCore.Text {
             return input.substring(0, length) + suffix;
         }
 
-        public static concatenate(parts:any[], seperator:string = ', ', lastSeparator:string = seperator):string {
+        /**
+         * Concatenate parts together.
+         *
+         * @param parts             Parts that get concatenated.
+         * @param seperator         Separator value that by which the parts get concatenated.
+         * @param lastSeparator     Last separator to concatenate parts with. Defaults to separator.
+         * @returns {string}
+         */
+        public static concatenate(parts: any[], seperator: string = ', ', lastSeparator: string = seperator): string {
 
             var result = '';
 
@@ -51,11 +73,26 @@ module TSCore.Text {
             return result;
         }
 
+        /**
+         * Zero pad a string.
+         * TODO: What if input.length is greater than width?
+         *
+         * @param input     String to be padded.
+         * @param width     Total length of the string after being padded.
+         * @param zero      String to pad input with. Defaults to "0".
+         * @returns {string}
+         */
         public static zeroPad(input:string, width:number, zero:string = '0'):string {
 
             return input.length >= width ? input : new Array(width - input.length + 1).join(zero) + input;
         }
 
+        /**
+         * Make a string's first character uppercase.
+         *
+         * @param input String to be parsed.
+         * @returns {string}
+         */
         public static ucFirst(input:string):string {
 
             if (input == '') {
