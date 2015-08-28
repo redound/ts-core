6caeb6254ef5ac87c9341b574fc52e01ec74fa61
Olivier Andriessen <olivierandriessen@gmail.com>
2015-08-28 20:40:13 +0200

    Event: add comments

--- src/TSCore/Event/Event.ts
+++ src/TSCore/Event/Event.ts
@@ -4,16 +4,33 @@ module TSCore.Events {
 
         public isStopped: boolean;
 
+        /**
+         * Constructor function.
+         *
+         * @param topic Topic name of event.
+         * @param _params Params that will be passed along event.
+         * @param caller Context that trigger the original event.
+         */
         constructor(public topic: string, private _params: T, public caller: any) {
 
             this.isStopped = false;
         }
 
+        /**
+         * Magic getter for params.
+         *
+         * @returns {T}
+         */
         public get params(): T {
             return this._params;
         }
 
-        stop() {
+        /**
+         * Stop the event from being called.
+         *
+         * @returns {void}
+         */
+        stop(): void {
             this.isStopped = true;
         }
     }
