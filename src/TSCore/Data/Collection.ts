63a93e750c290c2ead7bfa76af91c2c1f0aa536a
Olivier Andriessen <olivierandriessen@gmail.com>
2015-08-29 21:20:33 +0200

    Collection: fix pass items itself instead inside array

--- src/TSCore/Data/Collection.ts
+++ src/TSCore/Data/Collection.ts
@@ -43,7 +43,7 @@ module TSCore.Data {
 
             this._data = items.concat(this._data);
 
-            this.trigger(CollectionEvents.ADD, { items: [items] });
+            this.trigger(CollectionEvents.ADD, { items: items });
             this.trigger(CollectionEvents.CHANGE);
         }
 
