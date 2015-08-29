2f7290a4e0309e43252c7e88197a10328d0d1540
Olivier Andriessen <olivierandriessen@gmail.com>
2015-08-29 10:04:11 +0200

    Add ModelCollection and RemoteCollection stub

---
+++ src/TSCore/Data/Collection/ModelCollection.ts
@@ -0,0 +1,8 @@
+/// <reference path="./Collection.ts" />
+
+module TSCore.Data.Collection {
+
+    export class ModelCollection extends Collection {
+
+    }
+}
---
+++ src/TSCore/Data/Collection/RemoteModelCollection.ts
@@ -0,0 +1,8 @@
+/// <reference path="./ModelCollection.ts" />
+
+module TSCore.Data.Collection {
+
+    export class RemoteModelCollection extends ModelCollection {
+
+    }
+}
