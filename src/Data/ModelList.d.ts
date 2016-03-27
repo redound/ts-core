cd1bd27910e2664fd5d07b186a6b7d152c9d3b5b
Olivier Andriessen <olivierandriessen@gmail.com>
2016-03-27 17:22:39 +0200

    Remove declaration files

--- src/Data/ModelList.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../typings/main.d.ts" />
-import Model from "./Model";
-import List from "./List";
-export default class ModelList<T extends Model> extends List<T> {
-}
--- src/Utils/Enum.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../typings/main.d.ts" />
-export default class Enum {
-    static names(e: any): string[];
-    static values(e: any): number[];
-    static object(e: any): {
-        name: string;
-        value: number;
-    }[];
-}
